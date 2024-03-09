f519b5bf44e9eda6f46376a55e7c952f92d3d0e9
mrdavtan <mrdavtan@protonmail.com>
2024-03-09 15:40:36 -0800

    fixed -v flag, removed logs

--- tests/txt2kb.py
+++ tests/txt2kb.py
@@ -1,6 +1,6 @@
-#!/bin/bash
-#pip install transformers wikipedia newspaper3k GoogleNews pyvis
-
+#!/bin/env python3
+import argparse
+import logging
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 import math
 import torch
@@ -10,10 +10,9 @@ from GoogleNews import GoogleNews
 import IPython
 from pyvis.network import Network
 
-# Load model and tokenizer
-tokenizer = AutoTokenizer.from_pretrained("Babelscape/rebel-large")
-model = AutoModelForSeq2SeqLM.from_pretrained("Babelscape/rebel-large")
-
+def read_text_from_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
 
 def extract_relations_from_model_output(text):
     relations = []
@@ -95,6 +94,7 @@ class KB():
 
     def add_relation(self, r):
         # check on wikipedia
+        print("Attempting to add relation:", r)
         candidate_entities = [r["head"], r["tail"]]
         entities = [self.get_wikipedia_data(ent) for ent in candidate_entities]
 
@@ -144,7 +144,12 @@ def from_small_text_to_kb(text, verbose=False):
         **model_inputs,
         **gen_kwargs,
     )
+
+    logging.info("Generated tokens:", generated_tokens)
+
     decoded_preds = tokenizer.batch_decode(generated_tokens, skip_special_tokens=False)
+
+    logging.info("Decoded predictions:", decoded_preds)
 
     # create kb
     for sentence_pred in decoded_preds:
@@ -156,6 +161,7 @@ def from_small_text_to_kb(text, verbose=False):
 
 
 def from_text_to_kb(text, span_length=128, verbose=False):
+    logging.debug("Starting to process text for KB creation")
     # tokenize whole text
     inputs = tokenizer([text], return_tensors="pt")
 
@@ -201,8 +207,10 @@ def from_text_to_kb(text, span_length=128, verbose=False):
     )
 
     # decode relations
+    print("Generated tokens:", generated_tokens)
     decoded_preds = tokenizer.batch_decode(generated_tokens,
                                            skip_special_tokens=False)
+    print("Decoded predictions:", decoded_preds)
 
     # create kb
     kb = KB()
@@ -219,14 +227,46 @@ def from_text_to_kb(text, span_length=128, verbose=False):
 
     return kb
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.getLogger("urllib3").setLevel(logging.WARNING)
 
-text = """
-Napoleon Bonaparte (born Napoleone di Buonaparte; 15 August 1769 – 5 May 1821), and later known by his regnal name Napoleon I, was a French military and political leader who rose to prominence during the French Revolution and led several successful campaigns during the Revolutionary Wars. He was the de facto leader of the French Republic as First Consul from 1799 to 1804. As Napoleon I, he was Emperor of the French from 1804 until 1814 and again in 1815. Napoleon's political and cultural legacy has endured, and he has been one of the most celebrated and controversial leaders in world history. Napoleon was born on the island of Corsica not long after its annexation by the Kingdom of France.[5] He supported the French Revolution in 1789 while serving in the French army, and tried to spread its ideals to his native Corsica. He rose rapidly in the Army after he saved the governing French Directory by firing on royalist insurgents. In 1796, he began a military campaign against the Austrians and their Italian allies, scoring decisive victories and becoming a national hero. Two years later, he led a military expedition to Egypt that served as a springboard to political power. He engineered a coup in November 1799 and became First Consul of the Republic. Differences with the British meant that the French faced the War of the Third Coalition by 1805. Napoleon shattered this coalition with victories in the Ulm Campaign, and at the Battle of Austerlitz, which led to the dissolving of the Holy Roman Empire. In 1806, the Fourth Coalition took up arms against him because Prussia became worried about growing French influence on the continent. Napoleon knocked out Prussia at the battles of Jena and Auerstedt, marched the Grande Armée into Eastern Europe, annihilating the Russians in June 1807 at Friedland, and forcing the defeated nations of the Fourth Coalition to accept the Treaties of Tilsit. Two years later, the Austrians challenged the French again during the War of the Fifth Coalition, but Napoleon solidified his grip over Europe after triumphing at the Battle of Wagram. Hoping to extend the Continental System, his embargo against Britain, Napoleon invaded the Iberian Peninsula and declared his brother Joseph King of Spain in 1808. The Spanish and the Portuguese revolted in the Peninsular War, culminating in defeat for Napoleon's marshals. Napoleon launched an invasion of Russia in the summer of 1812. The resulting campaign witnessed the catastrophic retreat of Napoleon's Grande Armée. In 1813, Prussia and Austria joined Russian forces in a Sixth Coalition against France. A chaotic military campaign resulted in a large coalition army defeating Napoleon at the Battle of Leipzig in October 1813. The coalition invaded France and captured Paris, forcing Napoleon to abdicate in April 1814. He was exiled to the island of Elba, between Corsica and Italy. In France, the Bourbons were restored to power. However, Napoleon escaped Elba in February 1815 and took control of France.[6][7] The Allies responded by forming a Seventh Coalition, which defeated Napoleon at the Battle of Waterloo in June 1815. The British exiled him to the remote island of Saint Helena in the Atlantic, where he died in 1821 at the age of 51. Napoleon had an extensive impact on the modern world, bringing liberal reforms to the many countries he conquered, especially the Low Countries, Switzerland, and parts of modern Italy and Germany. He implemented liberal policies in France and Western Europe.
-"""
+tokenizer = AutoTokenizer.from_pretrained("Babelscape/rebel-large")
+model = AutoModelForSeq2SeqLM.from_pretrained("Babelscape/rebel-large")
 
-kb = from_text_to_kb(text, verbose=True)
-kb.print()
+def main():
 
+    try:
 
+        # Define command line arguments
+        parser = argparse.ArgumentParser(description='Extract knowledge base from text in a file.')
+        parser.add_argument('file_path', type=str, help='Path to the text file to be processed')
+        parser.add_argument('-v', '--verbose', action='store_true', help='Increase output verbosity')
+        args = parser.parse_args()
 
+        # Configure logging based on the verbose argument
+        if args.verbose:
+            logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+        else:
+            # Only log warnings and above if not in verbose mode
+            logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s')
+
+        # Reading text from the file
+        text = read_text_from_file(args.file_path)  # Direct use of 'text' as per function expectations
+        logging.debug("Text successfully read from the file.")
+        #logging.debug("text= ", text)
+
+        relations = extract_relations_from_model_output(text)
+        logging.debug(f"Extracted {len(relations)} relations from the text.")
+
+        # Process the text to extract knowledge base
+        kb = from_text_to_kb(text, verbose=True)
+        kb.print()
+
+    except Exception as e:
+        #print(f"Error: {e}")
+        logging.error(f"Error: {e}", exc_info=True)
+        raise  # This will print the stack trace.
+
+if __name__ == "__main__":
+    main()
 
--- txt2kb/txt2kb.py
+++ txt2kb/txt2kb.py
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/env python3
 import argparse
+import logging
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 import math
 import torch
@@ -9,233 +10,262 @@ from GoogleNews import GoogleNews
 import IPython
 from pyvis.network import Network
 
+def read_text_from_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def extract_relations_from_model_output(text):
+    relations = []
+    relation, subject, relation, object_ = '', '', '', ''
+    text = text.strip()
+    current = 'x'
+    text_replaced = text.replace("<s>", "").replace("<pad>", "").replace("</s>", "")
+    for token in text_replaced.split():
+        if token == "<triplet>":
+            current = 't'
+            if relation != '':
+                relations.append({
+                    'head': subject.strip(),
+                    'type': relation.strip(),
+                    'tail': object_.strip()
+                })
+                relation = ''
+            subject = ''
+        elif token == "<subj>":
+            current = 's'
+            if relation != '':
+                relations.append({
+                    'head': subject.strip(),
+                    'type': relation.strip(),
+                    'tail': object_.strip()
+                })
+            object_ = ''
+        elif token == "<obj>":
+            current = 'o'
+            relation = ''
+        else:
+            if current == 't':
+                subject += ' ' + token
+            elif current == 's':
+                object_ += ' ' + token
+            elif current == 'o':
+                relation += ' ' + token
+    if subject != '' and relation != '' and object_ != '':
+        relations.append({
+            'head': subject.strip(),
+            'type': relation.strip(),
+            'tail': object_.strip()
+        })
+    return relations
+
+
+class KB():
+    def __init__(self):
+        self.relations = []
+        self.entities = {}
+
+    def are_relations_equal(self, r1, r2):
+        return all(r1[attr] == r2[attr] for attr in ["head", "type", "tail"])
+
+    def exists_relation(self, r1):
+        return any(self.are_relations_equal(r1, r2) for r2 in self.relations)
+
+    def merge_relations(self, r1):
+        r2 = [r for r in self.relations
+              if self.are_relations_equal(r1, r)][0]
+        spans_to_add = [span for span in r1["meta"]["spans"]
+                        if span not in r2["meta"]["spans"]]
+        r2["meta"]["spans"] += spans_to_add
+
+    def get_wikipedia_data(self, candidate_entity):
+        try:
+            page = wikipedia.page(candidate_entity, auto_suggest=False)
+            entity_data = {
+                "title": page.title,
+                "url": page.url,
+                "summary": page.summary
+            }
+            return entity_data
+        except:
+            return None
+
+    def add_entity(self, e):
+        self.entities[e["title"]] = {k:v for k,v in e.items() if k != "title"}
+
+    def add_relation(self, r):
+        # check on wikipedia
+        print("Attempting to add relation:", r)
+        candidate_entities = [r["head"], r["tail"]]
+        entities = [self.get_wikipedia_data(ent) for ent in candidate_entities]
+
+        # if one entity does not exist, stop
+        if any(ent is None for ent in entities):
+            return
+
+        # manage new entities
+        for e in entities:
+            self.add_entity(e)
+
+        # rename relation entities with their wikipedia titles
+        r["head"] = entities[0]["title"]
+        r["tail"] = entities[1]["title"]
+
+        # manage new relation
+        if not self.exists_relation(r):
+            self.relations.append(r)
+        else:
+            self.merge_relations(r)
+
+    def print(self):
+        print("Entities:")
+        for e in self.entities.items():
+            print(f"  {e}")
+        print("Relations:")
+        for r in self.relations:
+            print(f"  {r}")
+
+def from_small_text_to_kb(text, verbose=False):
+    kb = KB()
+
+    # Tokenizer text
+    model_inputs = tokenizer(text, max_length=512, padding=True, truncation=True,
+                            return_tensors='pt')
+    if verbose:
+        print(f"Num tokens: {len(model_inputs['input_ids'][0])}")
+
+    # Generate
+    gen_kwargs = {
+        "max_length": 216,
+        "length_penalty": 0,
+        "num_beams": 3,
+        "num_return_sequences": 3
+    }
+    generated_tokens = model.generate(
+        **model_inputs,
+        **gen_kwargs,
+    )
+
+    logging.info("Generated tokens:", generated_tokens)
+
+    decoded_preds = tokenizer.batch_decode(generated_tokens, skip_special_tokens=False)
+
+    logging.info("Decoded predictions:", decoded_preds)
+
+    # create kb
+    for sentence_pred in decoded_preds:
+        relations = extract_relations_from_model_output(sentence_pred)
+        for r in relations:
+            kb.add_relation(r)
+
+    return kb
+
+
+def from_text_to_kb(text, span_length=128, verbose=False):
+    logging.debug("Starting to process text for KB creation")
+    # tokenize whole text
+    inputs = tokenizer([text], return_tensors="pt")
+
+    # compute span boundaries
+    num_tokens = len(inputs["input_ids"][0])
+    if verbose:
+        print(f"Input has {num_tokens} tokens")
+    num_spans = math.ceil(num_tokens / span_length)
+    if verbose:
+        print(f"Input has {num_spans} spans")
+    overlap = math.ceil((num_spans * span_length - num_tokens) /
+                        max(num_spans - 1, 1))
+    spans_boundaries = []
+    start = 0
+    for i in range(num_spans):
+        spans_boundaries.append([start + span_length * i,
+                                 start + span_length * (i + 1)])
+        start -= overlap
+    if verbose:
+        print(f"Span boundaries are {spans_boundaries}")
+
+    # transform input with spans
+    tensor_ids = [inputs["input_ids"][0][boundary[0]:boundary[1]]
+                  for boundary in spans_boundaries]
+    tensor_masks = [inputs["attention_mask"][0][boundary[0]:boundary[1]]
+                    for boundary in spans_boundaries]
+    inputs = {
+        "input_ids": torch.stack(tensor_ids),
+        "attention_mask": torch.stack(tensor_masks)
+    }
+
+    # generate relations
+    num_return_sequences = 3
+    gen_kwargs = {
+        "max_length": 256,
+        "length_penalty": 0,
+        "num_beams": 3,
+        "num_return_sequences": num_return_sequences
+    }
+    generated_tokens = model.generate(
+        **inputs,
+        **gen_kwargs,
+    )
+
+    # decode relations
+    print("Generated tokens:", generated_tokens)
+    decoded_preds = tokenizer.batch_decode(generated_tokens,
+                                           skip_special_tokens=False)
+    print("Decoded predictions:", decoded_preds)
+
+    # create kb
+    kb = KB()
+    i = 0
+    for sentence_pred in decoded_preds:
+        current_span_index = i // num_return_sequences
+        relations = extract_relations_from_model_output(sentence_pred)
+        for relation in relations:
+            relation["meta"] = {
+                "spans": [spans_boundaries[current_span_index]]
+            }
+            kb.add_relation(relation)
+        i += 1
+
+    return kb
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+tokenizer = AutoTokenizer.from_pretrained("Babelscape/rebel-large")
+model = AutoModelForSeq2SeqLM.from_pretrained("Babelscape/rebel-large")
 
 def main():
-    parser = argparse.ArgumentParser(description='Extract knowledge base from text in a file.')
-    parser.add_argument('file_path', type=str, help='Path to the text file to be processed')
-    args = parser.parse_args()
 
+    try:
 
-    def read_text_from_file(file_path):
-        with open(file_path, 'r', encoding='utf-8') as file:
-            return file.read()
+        # Define command line arguments
+        parser = argparse.ArgumentParser(description='Extract knowledge base from text in a file.')
+        parser.add_argument('file_path', type=str, help='Path to the text file to be processed')
+        parser.add_argument('-v', '--verbose', action='store_true', help='Increase output verbosity')
+        args = parser.parse_args()
 
-    def extract_relations_from_model_output(text):
-        relations = []
-        relation, subject, relation, object_ = '', '', '', ''
-        text = text.strip()
-        current = 'x'
-        text_replaced = text.replace("<s>", "").replace("<pad>", "").replace("</s>", "")
-        for token in text_replaced.split():
-            if token == "<triplet>":
-                current = 't'
-                if relation != '':
-                    relations.append({
-                        'head': subject.strip(),
-                        'type': relation.strip(),
-                        'tail': object_.strip()
-                    })
-                    relation = ''
-                subject = ''
-            elif token == "<subj>":
-                current = 's'
-                if relation != '':
-                    relations.append({
-                        'head': subject.strip(),
-                        'type': relation.strip(),
-                        'tail': object_.strip()
-                    })
-                object_ = ''
-            elif token == "<obj>":
-                current = 'o'
-                relation = ''
-            else:
-                if current == 't':
-                    subject += ' ' + token
-                elif current == 's':
-                    object_ += ' ' + token
-                elif current == 'o':
-                    relation += ' ' + token
-        if subject != '' and relation != '' and object_ != '':
-            relations.append({
-                'head': subject.strip(),
-                'type': relation.strip(),
-                'tail': object_.strip()
-            })
-        return relations
+        # Configure logging based on the verbose argument
+        if args.verbose:
+            logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+        else:
+            # Only log warnings and above if not in verbose mode
+            logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s')
 
+        # Reading text from the file
+        text = read_text_from_file(args.file_path)  # Direct use of 'text' as per function expectations
+        logging.debug("Text successfully read from the file.")
+        #logging.debug("text= ", text)
 
-    class KB():
-        def __init__(self):
-            self.relations = []
-            self.entities = {}
-
-        def are_relations_equal(self, r1, r2):
-            return all(r1[attr] == r2[attr] for attr in ["head", "type", "tail"])
-
-        def exists_relation(self, r1):
-            return any(self.are_relations_equal(r1, r2) for r2 in self.relations)
-
-        def merge_relations(self, r1):
-            r2 = [r for r in self.relations
-                  if self.are_relations_equal(r1, r)][0]
-            spans_to_add = [span for span in r1["meta"]["spans"]
-                            if span not in r2["meta"]["spans"]]
-            r2["meta"]["spans"] += spans_to_add
-
-        def get_wikipedia_data(self, candidate_entity):
-            try:
-                page = wikipedia.page(candidate_entity, auto_suggest=False)
-                entity_data = {
-                    "title": page.title,
-                    "url": page.url,
-                    "summary": page.summary
-                }
-                return entity_data
-            except:
-                return None
-
-        def add_entity(self, e):
-            self.entities[e["title"]] = {k:v for k,v in e.items() if k != "title"}
-
-        def add_relation(self, r):
-            # check on wikipedia
-            candidate_entities = [r["head"], r["tail"]]
-            entities = [self.get_wikipedia_data(ent) for ent in candidate_entities]
-
-            # if one entity does not exist, stop
-            if any(ent is None for ent in entities):
-                return
-
-            # manage new entities
-            for e in entities:
-                self.add_entity(e)
-
-            # rename relation entities with their wikipedia titles
-            r["head"] = entities[0]["title"]
-            r["tail"] = entities[1]["title"]
-
-            # manage new relation
-            if not self.exists_relation(r):
-                self.relations.append(r)
-            else:
-                self.merge_relations(r)
-
-        def print(self):
-            print("Entities:")
-            for e in self.entities.items():
-                print(f"  {e}")
-            print("Relations:")
-            for r in self.relations:
-                print(f"  {r}")
-
-    def from_small_text_to_kb(text, verbose=False):
-        kb = KB()
-
-        # Tokenizer text
-        model_inputs = tokenizer(text, max_length=512, padding=True, truncation=True,
-                                return_tensors='pt')
-        if verbose:
-            print(f"Num tokens: {len(model_inputs['input_ids'][0])}")
-
-        # Generate
-        gen_kwargs = {
-            "max_length": 216,
-            "length_penalty": 0,
-            "num_beams": 3,
-            "num_return_sequences": 3
-        }
-        generated_tokens = model.generate(
-            **model_inputs,
-            **gen_kwargs,
-        )
-        decoded_preds = tokenizer.batch_decode(generated_tokens, skip_special_tokens=False)
-
-        # create kb
-        for sentence_pred in decoded_preds:
-            relations = extract_relations_from_model_output(sentence_pred)
-            for r in relations:
-                kb.add_relation(r)
-
-        return kb
-
-
-    def from_text_to_kb(text, span_length=128, verbose=False):
-        # tokenize whole text
-        inputs = tokenizer([text], return_tensors="pt")
-
-        # compute span boundaries
-        num_tokens = len(inputs["input_ids"][0])
-        if verbose:
-            print(f"Input has {num_tokens} tokens")
-        num_spans = math.ceil(num_tokens / span_length)
-        if verbose:
-            print(f"Input has {num_spans} spans")
-        overlap = math.ceil((num_spans * span_length - num_tokens) /
-                            max(num_spans - 1, 1))
-        spans_boundaries = []
-        start = 0
-        for i in range(num_spans):
-            spans_boundaries.append([start + span_length * i,
-                                     start + span_length * (i + 1)])
-            start -= overlap
-        if verbose:
-            print(f"Span boundaries are {spans_boundaries}")
-
-        # transform input with spans
-        tensor_ids = [inputs["input_ids"][0][boundary[0]:boundary[1]]
-                      for boundary in spans_boundaries]
-        tensor_masks = [inputs["attention_mask"][0][boundary[0]:boundary[1]]
-                        for boundary in spans_boundaries]
-        inputs = {
-            "input_ids": torch.stack(tensor_ids),
-            "attention_mask": torch.stack(tensor_masks)
-        }
-
-        # generate relations
-        num_return_sequences = 3
-        gen_kwargs = {
-            "max_length": 256,
-            "length_penalty": 0,
-            "num_beams": 3,
-            "num_return_sequences": num_return_sequences
-        }
-        generated_tokens = model.generate(
-            **inputs,
-            **gen_kwargs,
-        )
-
-        # decode relations
-        decoded_preds = tokenizer.batch_decode(generated_tokens,
-                                               skip_special_tokens=False)
-
-        # create kb
-        kb = KB()
-        i = 0
-        for sentence_pred in decoded_preds:
-            current_span_index = i // num_return_sequences
-            relations = extract_relations_from_model_output(sentence_pred)
-            for relation in relations:
-                relation["meta"] = {
-                    "spans": [spans_boundaries[current_span_index]]
-                }
-                kb.add_relation(relation)
-            i += 1
-
-        return kb
-
-
-        # Load model and tokenizer
-        tokenizer = AutoTokenizer.from_pretrained("Babelscape/rebel-large")
-        model = AutoModelForSeq2SeqLM.from_pretrained("Babelscape/rebel-large")
-
-
-        # Read text from the file provided in command line argument
-        text = read_text_from_file(args.file_path)
+        relations = extract_relations_from_model_output(text)
+        logging.debug(f"Extracted {len(relations)} relations from the text.")
 
         # Process the text to extract knowledge base
         kb = from_text_to_kb(text, verbose=True)
         kb.print()
+
+    except Exception as e:
+        #print(f"Error: {e}")
+        logging.error(f"Error: {e}", exc_info=True)
+        raise  # This will print the stack trace.
 
 if __name__ == "__main__":
     main()
